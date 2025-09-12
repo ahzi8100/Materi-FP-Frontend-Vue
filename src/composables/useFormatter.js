634932e2393974a09ae517ee557643a9486e7718
Ahmad Fauzi <uzif8100@gmail.com>
2025-09-12 14:48:39 +0700

    Update moneyFormat

--- src/composables/useFormatter.js
+++ src/composables/useFormatter.js
@@ -1,27 +1,28 @@
 export function moneyFormat(number) {
   // Tambahkan pengecekan untuk menghindari error jika data tidak lengkap
-  let num = Number(number);
+  // let num = Number(number);
 
-  if (isNaN(num)) {
-    return "0"; // fallback kalau datanya tidak valid
-  }
+  // if (isNaN(num)) {
+  //   return "0"; // fallback kalau datanya tidak valid
+  // }
 
-  let val = num.toFixed(2).replace('.', ',');
-  return val.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  // let val = num.toFixed(2).replace('.', ',');
+  // return val.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-  // if (typeof number !== 'number') {
-  //   return parseInt(number)
-  // };
+  if (typeof number !== 'number') {
+    return parseInt(number)
+  };
 
-  // let val = (number / 1).toFixed(2).replace('.', ',');
-  // return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  let val = (number / 1).toFixed(2).replace('.', ',');
+  return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
 // calculate discount
 export function calculateDiscount(product) {
   // Tambahkan pengecekan untuk menghindari error jika data tidak lengkap
   if (!product || typeof product.price !== 'number' || typeof product.discount !== 'number') {
-    return '';
+    parseInt(product.price);
+    parseInt(product.discount);
   }
 
   return product.price - (product.price * (product.discount) / 100);
